96e3b3c0137541854a81ea5ef729c9610add64ef
EsVagy <sltamagotchi@gmail.com>
2024-05-18 16:07:05 +0000

    Added macros

---
+++ macros.h
@@ -0,0 +1,17 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define NEW(type, ...)                                                         \
+  ({                                                                           \
+    type _val;                                                                 \
+    init_##type(&_val, __VA_ARGS__) _val;                                      \
+  })
+
+#define DELETE(var) {free_##typeof(var)(&var)};
+
+#define CLEANUP(type) __attribute__((__cleanup__(free_##type)))
+
+#define ARR_SIZE(arr) (sizeof(arr) / sizeof(*arr))
+
+#endif
+
