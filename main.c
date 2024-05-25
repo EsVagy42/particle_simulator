ff28112d7175da61ff5f6f1d0db60c0be15d3f1d
EsVagy <sltamagotchi@gmail.com>
2024-05-25 17:29:33 +0000

    Made main.c web compatible

--- main.c
+++ main.c
@@ -4,16 +4,38 @@
 #include "simulator_structs.h"
 #include <raylib.h>
 
+#if defined(PLATFORMWEB)
+  #include <emscripten/emscripten.h>
+#endif
+
 extern void SandInit(Generation *, Cell *);
+Generation generation;
+
+void UpdateDrawFrame();
 
 int main() {
   const Size2 SIMULATOR_SIZE = {400, 300};
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "Particle Simulator V3");
-  Generation generation AUTODEL(Generation) = NEW(Generation, SIMULATOR_SIZE);
+  generation = NEW(Generation, SIMULATOR_SIZE);
 
+#if defined(PLATFORMWEB)
+  emsripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
   while (!WindowShouldClose()) {
+    UpdateDrawFrame();
+  }
+#endif
+
+  DeleteGeneration(&generation);
+
+  CloseWindow();
+
+  return 0;
+}
+
+void UpdateDrawFrame() {
     SandInit(&generation, CELL((&generation), ((Position2){200, 50})));
     
     update_gen(&generation);
@@ -25,9 +47,4 @@ int main() {
       DrawFPS(0, 0);
     }
     EndDrawing();
-  }
-
-  CloseWindow();
-
-  return 0;
 }
