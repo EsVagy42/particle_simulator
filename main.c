c78cbf0899007a5a67d469685a94d85a6b055c87
EsVagy <sltamagotchi@gmail.com>
2024-05-25 18:44:21 +0000

    Set target FPS to 60 on desktop

--- main.c
+++ main.c
@@ -23,6 +23,8 @@ int main() {
 #if defined(PLATFORMWEB)
   emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
+  SetTargetFPS(60);
+  
   while (!WindowShouldClose()) {
     UpdateDrawFrame();
   }
