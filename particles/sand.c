6e02881e4d2ab0677ecca67270d99062d12459bd
EsVagy <sltamagotchi@gmail.com>
2024-05-27 17:17:08 +0000

    Changed sand implementation

--- particles/sand.c
+++ particles/sand.c
@@ -1,5 +1,4 @@
 #include "../particle_includes.h"
-#include "particles.h"
 
 CREATE_PARTICLE(Sand);
 
@@ -13,19 +12,8 @@ void SandInit(Generation *gen, Cell *cell) {
 
 void SandUpdate(Generation *gen, Cell *cell) {
   static Position2 possible_moves[] = {{0, 1}, {1, 1}, {-1, 1}};
-  for (Position2 *move = possible_moves; move < ARREND(possible_moves);
-       move++) {
-    Position2 cell_pos = ADD_POS(cell->position, (*move));
-    if (!IS_POS_VALID(gen->size, cell_pos)) {
-      continue;
-    }
-    Cell *other_cell = CELL(gen, cell_pos);
-    if (!sand_swappable_particles[other_cell->type]) {
-      continue;
-    }
-    swap_cells(gen, cell, other_cell);
-    *CELL_COLOR(gen, cell->position) = YELLOW;
-  }
+  TRY_SWAP(gen, cell, possible_moves, sand_swappable_particles[_cell->type]);
+  *CELL_COLOR(gen, cell->position) = YELLOW;
 }
 
 ADD_PARTICLE(Sand);
